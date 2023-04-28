57fd9d48ad0bf580957e545e4c85e2059057220d
csunny <cfqcsunny@gmail.com>
2023-04-28 22:18:08 +0800

    init model and tokenizer

---
+++ pilot/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__version__ = "0.0.1"
--- pilot/server/sqlgpt.py
+++ pilot/server/sqlgpt.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-from model.loader import ModerLoader
+from pilot.model.loader import ModerLoader
 from fastchat.serve.inference import generate_stream
-from configs.model_config import *
+from pilot.configs.model_config import *
 
 if __name__ == "__main__":
-    pass
+
+    model_path = llm_model_config[LLM_MODEL]
+
+    ml = ModerLoader(model_path)
+    model, tokenizer = ml.loader(load_8bit=True) 
+    print(model)
+    print(tokenizer)
