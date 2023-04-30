6e480c2a741c792db90bf734c8197bcf94c1ea7e
csunny <cfqcsunny@gmail.com>
2023-04-30 14:21:58 +0800

    checkout load_model to fastchat

--- pilot/server/vicuna_server.py
+++ pilot/server/vicuna_server.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import uvicorn
 from typing import Optional, List
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pilot.model.inference import generate_output, get_embeddings
+from fastchat.serve.inference import load_model
 from pilot.model.loader import ModerLoader
 from pilot.configs.model_config import *
 
 model_path = llm_model_config[LLM_MODEL] 
-ml = ModerLoader(model_path=model_path)
-model, tokenizer = ml.loader(load_8bit=isload_8bit, debug=isdebug)
+# ml = ModerLoader(model_path=model_path)
+# model, tokenizer = ml.loader(load_8bit=isload_8bit, debug=isdebug)
 
+model, tokenizer = load_model(model_path=model_path, device=DEVICE, num_gpus=1, load_8bit=True, debug=False)
 app = FastAPI()
 
 class PromptRequest(BaseModel):
@@ -46,3 +49,8 @@ def embeddings(prompt_request: EmbeddingRequest):
     print("Received prompt: ", params["prompt"])
     output = get_embeddings(model, tokenizer, params["prompt"])
     return {"response": [float(x) for x in output]}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", log_level="info") 
