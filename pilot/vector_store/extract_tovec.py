205eab72683210244ed2a63e7a3b25bbb637c257
csunny <cfqcsunny@gmail.com>
2023-05-05 21:40:23 +0800

    sentence transformer

--- pilot/vector_store/extract_tovec.py
+++ pilot/vector_store/extract_tovec.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import os
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from pilot.model.vicuna_llm import VicunaEmbeddingLLM
-# from langchain.embeddings import SentenceTransformerEmbeddings
-
+from pilot.configs.model_config import VECTORE_PATH
+from langchain.embeddings import HuggingFaceEmbeddings
 
 embeddings = VicunaEmbeddingLLM()
 
@@ -21,18 +22,22 @@ def knownledge_tovec(filename):
     )
     return docsearch
 
+def knownledge_tovec_st(filename):
+    """ Use sentence transformers to embedding the document.
+        https://github.com/UKPLab/sentence-transformers
+    """
+    from pilot.configs.model_config import llm_model_config
+    embeddings = HuggingFaceEmbeddings(model=llm_model_config["sentence-transforms"])
 
-# def knownledge_tovec_st(filename):
-#     """ Use sentence transformers to embedding the document.
-#         https://github.com/UKPLab/sentence-transformers
-#     """
-#     from pilot.configs.model_config import llm_model_config
-#     embeddings = SentenceTransformerEmbeddings(model=llm_model_config["sentence-transforms"])
-
-#     with open(filename, "r") as f:
-#         knownledge = f.read()
+    with open(filename, "r") as f:
+        knownledge = f.read()
     
-#     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-#     texts = text_splitter(knownledge)
-#     docsearch = Chroma.from_texts(texts, embeddings, metadatas=[{"source": str(i)} for i in range(len(texts))])
-#     return docsearch
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter(knownledge)
+    docsearch = Chroma.from_texts(texts, embeddings, metadatas=[{"source": str(i)} for i in range(len(texts))])
+    return docsearch
+
+def get_vector_storelist():
+    if not os.path.exists(VECTORE_PATH):
+        return []
+    return os.listdir(VECTORE_PATH)
