e48087e0452c6ad3fe952c58869ec51e73c787eb
csunny <cfqcsunny@gmail.com>
2023-05-09 00:15:19 +0800

    update config

--- pilot/agent/agent_manager.py
+++ pilot/agent/agent_manager.py
@@ -2,14 +2,34 @@
 # -*- coding:utf-8 -*-
 
 from pilot.singleton import Singleton
+from pilot.configs.config import Config
 
 class AgentManager(metaclass=Singleton):
-    """Agent manager for managing DB-GPT agents"""
-    def __init__(self) -> None:
-        
-        self.agents = {} #TODO need to define
+    """Agent manager for managing DB-GPT agents
+       In order to compatible auto gpt plugins, 
+       we use the same template with it.
+    
+        Args: next_keys
+                agents
+                cfg
+    """
 
-    def create_agent(self):
+    def __init__(self) -> None:
+        self.next_key = 0
+        self.agents = {} #TODO need to define
+        self.cfg = Config()
+
+    def create_agent(self, task: str, prompt: str, model: str) -> tuple[int, str]:
+        """Create a new agent and return its key
+    
+        Args:
+            task: The task to perform
+            prompt: The prompt to use
+            model: The model to use
+
+        Returns:
+            The key of the new agent
+        """
         pass
 
     def message_agent(self):
--- pilot/configs/config.py
+++ pilot/configs/config.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+from typing import List
+
 from auto_gpt_plugin_template import AutoGPTPluginTemplate
 from pilot.singleton import Singleton
 
@@ -8,7 +11,22 @@ class Config(metaclass=Singleton):
     """Configuration class to store the state of bools for different scripts access"""
     def __init__(self) -> None:
         """Initialize the Config class"""
-        pass
 
+        # TODO change model_config there
+        self.debug_mode = False
+        self.execute_local_commands = (
+            os.getenv("EXECUTE_LOCAL_COMMANDS", "False") == "True"
+        )
 
-    # TODO change model_config there
+        self.plugins_dir = os.getenv("PLUGINS_DIR", 'plugins')
+        self.plugins:List[AutoGPTPluginTemplate] = []
+
+    def set_debug_mode(self, value: bool) -> None:
+        """Set the debug mode value."""
+        self.debug_mode = value
+
+    def set_plugins(self,value: bool) -> None:
+        """Set the plugins value."""
+        self.plugins = value
+
+  
