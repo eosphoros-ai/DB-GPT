d42a9f3bd147745e285338f5637b928f18f1eff9
chenketing <chenketing.ckt@antgroup.com>
2023-05-11 23:48:56 +0800

    feature:url,csv embedding

--- pilot/__init__.py
+++ pilot/__init__.py
@@ -1,11 +1,7 @@
 from pilot.source_embedding import (SourceEmbedding, register)
-from pilot.source_embedding import TextToVector
-from pilot.source_embedding import Text2Vectors
 
 
 __all__ = [
     "SourceEmbedding",
-    "TextToVector",
-    "Text2Vectors",
     "register"
 ]
