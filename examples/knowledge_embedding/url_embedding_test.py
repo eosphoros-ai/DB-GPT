1a0e3861372f84c18dc4ed37217c33f4e24516f4
chenketing <chenketing.ckt@antgroup.com>
2023-05-15 00:18:06 +0800

    fix:unusual file

--- examples/knowledge_embedding/url_embedding_test.py
+++ examples/knowledge_embedding/url_embedding_test.py
@@ -1,8 +1,8 @@
 from pilot.source_embedding.url_embedding import URLEmbedding
 
 path = "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-february-8-2023"
-model_name = "/Users/chenketing/Desktop/project/all-MiniLM-L6-v2"
-vector_store_path = "/pilot/source_embedding/"
+model_name = "your_path/all-MiniLM-L6-v2"
+vector_store_path = "your_path"
 
 
 pdf_embedding = URLEmbedding(file_path=path, model_name=model_name, vector_store_config={"vector_store_name": "url", "vector_store_path": "vector_store_path"})
