b360df7b9b738ad4e2764c770a5a4079caf19a40
csunny <cfqsunny@163.com>
2023-05-18 17:28:06 +0800

    fix run path

--- pilot/__init__.py
+++ pilot/__init__.py
@@ -1,5 +1,10 @@
 from pilot.source_embedding import (SourceEmbedding, register)
 
+import os
+import sys
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(ROOT_PATH)
 
 __all__ = [
     "SourceEmbedding",
