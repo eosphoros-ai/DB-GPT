92701c8081e0993348c1cf85b8adc272d09bbb70
aries-ckt <916701291@qq.com>
2023-05-19 21:17:39 +0800

    update:knowledge load script

--- pilot/source_embedding/chn_document_splitter.py
+++ pilot/source_embedding/chn_document_splitter.py
@@ -9,33 +9,17 @@ class CHNDocumentSplitter(CharacterTextSplitter):
         self.pdf = pdf
         self.sentence_size = sentence_size
 
-    # def split_text_version2(self, text: str) -> List[str]:
-    #     if self.pdf:
-    #         text = re.sub(r"\n{3,}", "\n", text)
-    #         text = re.sub('\s', ' ', text)
-    #         text = text.replace("\n\n", "")
-    #     sent_sep_pattern = re.compile('([﹒﹔﹖﹗．。！？]["’”」』]{0,2}|(?=["‘“「『]{1,2}|$))')  # del ：；
-    #     sent_list = []
-    #     for ele in sent_sep_pattern.split(text):
-    #         if sent_sep_pattern.match(ele) and sent_list:
-    #             sent_list[-1] += ele
-    #         elif ele:
-    #             sent_list.append(ele)
-    #     return sent_list
-
     def split_text(self, text: str) -> List[str]:
         if self.pdf:
             text = re.sub(r"\n{3,}", r"\n", text)
             text = re.sub('\s', " ", text)
             text = re.sub("\n\n", "", text)
 
-        text = re.sub(r'([;；.!?。！？\?])([^”’])', r"\1\n\2", text)  # 单字符断句符
-        text = re.sub(r'(\.{6})([^"’”」』])', r"\1\n\2", text)  # 英文省略号
-        text = re.sub(r'(\…{2})([^"’”」』])', r"\1\n\2", text)  # 中文省略号
+        text = re.sub(r'([;；.!?。！？\?])([^”’])', r"\1\n\2", text)
+        text = re.sub(r'(\.{6})([^"’”」』])', r"\1\n\2", text)
+        text = re.sub(r'(\…{2})([^"’”」』])', r"\1\n\2", text)
         text = re.sub(r'([;；!?。！？\?]["’”」』]{0,2})([^;；!?，。！？\?])', r'\1\n\2', text)
-        # 如果双引号前有终止符，那么双引号才是句子的终点，把分句符\n放到双引号后，注意前面的几句都小心保留了双引号
-        text = text.rstrip()  # 段尾如果有多余的\n就去掉它
-        # 很多规则中会考虑分号;，但是这里我把它忽略不计，破折号、英文双引号等同样忽略，需要的再做些简单调整即可。
+        text = text.rstrip()
         ls = [i for i in text.split("\n") if i]
         for ele in ls:
             if len(ele) > self.sentence_size:
