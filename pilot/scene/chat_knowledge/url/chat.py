e21bc69904ff5a3f8f068c229c3270a212919325
aries-ckt <916701291@qq.com>
2023-06-01 20:28:13 +0800

    feature:db_summary

--- pilot/scene/chat_knowledge/url/chat.py
+++ pilot/scene/chat_knowledge/url/chat.py
@@ -58,7 +58,6 @@ class ChatUrlKnowledge(BaseChat):
         docs = self.knowledge_embedding_client.similar_search(
             self.current_user_input, VECTOR_SEARCH_TOP_K
         )
-        docs = docs[:2000]
         context = [d.page_content for d in docs]
         context = context[:2000]
         input_values = {"context": context, "question": self.current_user_input}
