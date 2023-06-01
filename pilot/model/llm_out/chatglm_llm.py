eb6466fd87ca02fa7a4138bfd4052270dd03bdb7
yhjun1026 <460342015@qq.com>
2023-06-01 16:34:51 +0800

    model server fix message model

--- pilot/model/llm_out/chatglm_llm.py
+++ pilot/model/llm_out/chatglm_llm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import copy
 
 import torch
 
@@ -8,7 +9,7 @@ from pilot.conversation import ROLE_ASSISTANT, ROLE_USER
 
 @torch.inference_mode()
 def chatglm_generate_stream(
-    model, tokenizer, params, device, context_len=2048, stream_interval=2
+        model, tokenizer, params, device, context_len=2048, stream_interval=2
 ):
     """Generate text using chatglm model's chat api"""
     prompt = params["prompt"]
@@ -28,25 +29,34 @@ def chatglm_generate_stream(
         generate_kwargs["temperature"] = temperature
 
     # TODO, Fix this
-    hist = []
-
     messages = prompt.split(stop)
+    #
+    # # Add history conversation
+    hist = []
+    once_conversation = []
+    for message in messages:
+        if len(message) <= 0:
+            continue
 
-    # Add history chat to hist for model.
-    for i in range(1, len(messages) - 2, 2):
-        hist.append(
-            (
-                messages[i].split(ROLE_USER + ":")[1],
-                messages[i + 1].split(ROLE_ASSISTANT + ":")[1],
-            )
-        )
+        if "human:" in message:
+            once_conversation.append(message.split("human:")[1])
+        # elif "system:" in message:
+        #     once_conversation.append(f"""###system:{message.split("system:")[1]} """)
+        elif "ai:" in message:
+            once_conversation.append(message.split("ai:")[1])
+            last_conversation = copy.deepcopy(once_conversation)
+            hist.append(last_conversation)
+            once_conversation = []
+        # else:
+        #     once_conversation.append(f"""###system:{message} """)
 
-    query = messages[-2].split(ROLE_USER + ":")[1]
+    query = messages[-1].split("human:")[1]
     print("Query Message: ", query)
-    output = ""
-    i = 0
+    # output = ""
+    # i = 0
+
     for i, (response, new_hist) in enumerate(
-        model.stream_chat(tokenizer, query, hist, **generate_kwargs)
+            model.stream_chat(tokenizer, query, hist, **generate_kwargs)
     ):
         if echo:
             output = query + " " + response
