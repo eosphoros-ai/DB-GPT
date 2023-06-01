8027af61c326601c9daa16e6e319257850c0e460
csunny <cfqsunny@163.com>
2023-06-01 20:12:03 +0800

    fix: doc clear

--- pilot/server/webserver.py
+++ pilot/server/webserver.py
@@ -10,11 +10,11 @@ import uuid
 
 import gradio as gr
 
-from pilot.summary.db_summary_client import DBSummaryClient
 
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(ROOT_PATH)
 
+from pilot.summary.db_summary_client import DBSummaryClient
 from pilot.commands.command_mange import CommandRegistry
 
 from pilot.scene.base_chat import BaseChat
