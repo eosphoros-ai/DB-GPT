7136aa748dce63c2f1235d9606c2686d0437cc0f
ykgong <ykgong@B-T4ETMD6R-2046.local>
2023-06-09 13:57:47 +0800

    fix model key

--- pilot/configs/model_config.py
+++ pilot/configs/model_config.py
@@ -37,7 +37,7 @@ LLM_MODEL_CONFIG = {
     "guanaco-33b-merged": os.path.join(MODEL_PATH, "guanaco-33b-merged"),
     "falcon-40b": os.path.join(MODEL_PATH, "falcon-40b"),
     "gorilla-7b": os.path.join(MODEL_PATH, "gorilla-7b"),
-    "ggml-gpt4all-j-v1.3-groovy": os.path.join(MODEL_PATH, "ggml-gpt4all-j-v1.3-groovy.bin"),
+    "gptj-6b": os.path.join(MODEL_PATH, "ggml-gpt4all-j-v1.3-groovy.bin"),
     "proxyllm": "proxyllm",
 }
 
