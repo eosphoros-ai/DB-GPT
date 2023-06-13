9f9ff33b44f7822df3358a7c2ef47ad6107c91af
yhjun1026 <460342015@qq.com>
2023-06-13 21:03:21 +0800

    fix plugin mode bug；Optimize the parsing logic for model response

--- pilot/scene/chat_execution/out_parser.py
+++ pilot/scene/chat_execution/out_parser.py
@@ -21,6 +21,8 @@ class PluginChatOutputParser(BaseOutputParser):
     def parse_prompt_response(self, model_out_text) -> T:
         clean_json_str = super().parse_prompt_response(model_out_text)
         print(clean_json_str)
+        if not clean_json_str:
+            raise ValueError("model server response not have json!")
         try:
             response = json.loads(clean_json_str)
         except Exception as e:
