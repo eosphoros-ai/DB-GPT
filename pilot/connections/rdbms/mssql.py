ea635514307243f6588de8f132e424044a394848
csunny <cfqsunny@163.com>
2023-06-14 10:17:53 +0800

    pylint: multi model for gp4all (#138)

--- pilot/connections/rdbms/mssql.py
+++ pilot/connections/rdbms/mssql.py
@@ -5,9 +5,6 @@ from typing import Optional, Any
 from pilot.connections.rdbms.rdbms_connect import RDBMSDatabase
 
 
-
-
-
 class MSSQLConnect(RDBMSDatabase):
     """Connect MSSQL Database fetch MetaData
     Args:
@@ -18,6 +15,4 @@ class MSSQLConnect(RDBMSDatabase):
     dialect: str = "mssql"
     driver: str = "pyodbc"
 
-    default_db = ["master", "model", "msdb", "tempdb","modeldb", "resource"]
-
-
+    default_db = ["master", "model", "msdb", "tempdb", "modeldb", "resource"]
