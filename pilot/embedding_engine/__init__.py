083791ca3b12188dd51dc267902a8e3e69f12140
csunny <cfqsunny@163.com>
2023-06-20 19:36:35 +0800

    feat: define framework and split api add cache for llm add openapi module add proxy module

---
+++ pilot/embedding_engine/__init__.py
@@ -0,0 +1,3 @@
+from pilot.embedding_engine.source_embedding import SourceEmbedding, register
+
+__all__ = ["SourceEmbedding", "register"]
--- pilot/scene/chat_knowledge/custom/chat.py
+++ pilot/scene/chat_knowledge/custom/chat.py
@@ -17,7 +17,7 @@ from pilot.configs.model_config import (
 )
 
 from pilot.scene.chat_knowledge.custom.prompt import prompt
-from pilot.source_embedding.knowledge_embedding import KnowledgeEmbedding
+from pilot.embedding_engine.knowledge_embedding import KnowledgeEmbedding
 
 CFG = Config()
 
