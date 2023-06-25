d718c95d24538743fcd69f2c8d931cb8f4702117
changhuiping.chp <changhuiping.chp@digital-engine.com>
2023-06-25 18:09:53 +0800

    feat: 布局初版

--- datacenter/app/defaultTheme.ts
+++ datacenter/app/defaultTheme.ts
@@ -2,60 +2,62 @@ import { extendTheme } from '@mui/joy/styles';
 import colors from '@mui/joy/colors';
 
 export const joyTheme = extendTheme({
-    colorSchemes: {
-      light: {
-        palette: {
-          mode: 'dark',
-          primary: {
-            ...colors.purple,
-          },
-          neutral: {
-            plainColor: '#25252D',
-            plainHoverColor: '#131318',
-            plainHoverBg: '#EBEBEF',
-            plainActiveBg: '#D8D8DF',
-            plainDisabledColor: '#B9B9C6'
-          },
-          background: {
-            body: '#fff'
-          },
-          text: {
-            primary: '#25252D'
-          },
+  colorSchemes: {
+    light: {
+      palette: {
+        mode: 'dark',
+        primary: {
+          ...colors.purple,
         },
-      },
-      dark: {
-        palette: {
-          mode: 'light',
-          primary: {
-            ...colors.purple,
-          },
-          neutral: {
-            plainColor: '#D8D8DF',
-            plainHoverColor: '#F7F7F8',
-            plainHoverBg: '#25252D',
-            plainActiveBg: '#434356',
-            plainDisabledColor: '#434356'
-          },
-          text: {
-            primary: '#EBEBEF'
-          },
-          background: {
-            body: '#09090D'
-          }
+        neutral: {
+          plainColor: '#25252D',
+          plainHoverColor: '#131318',
+          plainHoverBg: '#EBEBEF',
+          plainActiveBg: '#D8D8DF',
+          plainDisabledColor: '#B9B9C6'
+        },
+        background: {
+          body: '#fff',
+          surface: '#fff'
+        },
+        text: {
+          primary: '#25252D'
         },
       },
     },
-    fontFamily: {
-      body: 'Josefin Sans, sans-serif',
-      display: 'Josefin Sans, sans-serif',
-    },
-    typography: {
-      display1: {
-        background:
-          'linear-gradient(-30deg, var(--joy-palette-primary-900), var(--joy-palette-primary-400))',
-        WebkitBackgroundClip: 'text',
-        WebkitTextFillColor: 'transparent',
+    dark: {
+      palette: {
+        mode: 'light',
+        primary: {
+          ...colors.purple,
+        },
+        neutral: {
+          plainColor: '#D8D8DF',
+          plainHoverColor: '#F7F7F8',
+          plainHoverBg: '#25252D',
+          plainActiveBg: '#434356',
+          plainDisabledColor: '#434356'
+        },
+        text: {
+          primary: '#EBEBEF'
+        },
+        background: {
+          body: '#09090D',
+          surface: '#1e293b40'
+        }
       },
     },
-  });
+  },
+  fontFamily: {
+    body: 'Josefin Sans, sans-serif',
+    display: 'Josefin Sans, sans-serif',
+  },
+  typography: {
+    display1: {
+      background:
+        'linear-gradient(-30deg, var(--joy-palette-primary-900), var(--joy-palette-primary-400))',
+      WebkitBackgroundClip: 'text',
+      WebkitTextFillColor: 'transparent',
+    },
+  },
+});
