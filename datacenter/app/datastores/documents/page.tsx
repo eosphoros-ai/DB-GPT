4063fcb7329340d391c9e5aa6b24a6b9c49b9387
shiweisong.ssw <shiweisong.ssw@digital-engine.com>
2023-06-26 16:34:58 +0800

    feat: fetch documents

---
+++ datacenter/app/datastores/documents/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { withRouter } from 'next/router'
+import React, { useState, useEffect } from 'react';
+import { Table } from 'antd';
+import moment from 'moment';
+
+const Documents = ({ router }) => {
+    const [documents, setDocuments] = useState<any>([]);
+    useEffect(() => {
+        async function fetchDocuments() {
+            const res = await fetch(`http://localhost:8000/knowledge/${router.query.name}/document/list`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({}),
+            });
+            const data = await res.json();
+            if (data.success) {
+                setDocuments(data.data);
+            }
+        }
+        fetchDocuments();
+    }, []);
+    return (
+        <div className='p-4'>
+            <Table
+                columns={[
+                    {
+                        title: 'Name',
+                        dataIndex: 'doc_name',
+                        key: 'doc_name',
+                        align: 'center',
+                    },
+                    {
+                        title: 'Type',
+                        dataIndex: 'doc_type',
+                        key: 'doc_type',
+                        align: 'center',
+                    },
+                    {
+                        title: 'Size',
+                        dataIndex: 'chunk_size',
+                        key: 'chunk_size',
+                        align: 'center',
+                    },
+                    {
+                        title: 'Last Synch',
+                        dataIndex: 'last_sync',
+                        key: 'last_sync',
+                        align: 'center',
+                        render: (text: string) => moment(text).format('YYYY-MM-DD HH:MM:SS')
+                    },
+                    {
+                        title: 'Status',
+                        dataIndex: 'status',
+                        key: 'status',
+                        align: 'center',
+                    },
+                ]}
+                dataSource={documents}
+            />
+        </div>
+    )
+}
+
+export default withRouter(Documents);
