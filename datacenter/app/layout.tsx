096f465478e62d50ee832fd041bdab7b7517507e
changhuiping.chp <changhuiping.chp@digital-engine.com>
2023-06-27 11:47:05 +0800

    fix: 添加跳转topbar提示

--- datacenter/app/layout.tsx
+++ datacenter/app/layout.tsx
@@ -1,19 +1,23 @@
 "use client"
 import './globals.css'
+import './nprogress.css';
 import LeftSider from '@/components/leftSider';
 import { CssVarsProvider, ThemeProvider } from '@mui/joy/styles';
 import { joyTheme } from './defaultTheme';
+import TopProgressBar from '@/components/topProgressBar';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}) { 
+
   return (
     <html lang="en" className="min-h-full font-sans">
       <body className={`min-h-screen font-sans`}>
         <ThemeProvider theme={joyTheme}>
           <CssVarsProvider theme={joyTheme} defaultMode="light">
+            <TopProgressBar />
             <div className='min-h-screen flex flex-col'>
               <div className="flex flex-1 flex-row">
                 <LeftSider />
