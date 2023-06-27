dac9e97e75437c4f84dcc9525e8f2484ae71e194
shiweisong.ssw <shiweisong.ssw@digital-engine.com>
2023-06-27 16:47:16 +0800

    feat: refactor the UI framework

--- datacenter/app/datastores/documents/chunklist/page.tsx
+++ datacenter/app/datastores/documents/chunklist/page.tsx
@@ -1,65 +1,76 @@
-"use client";
+'use client'
 
 import { useSearchParams } from 'next/navigation'
-import React, { useState, useEffect } from 'react';
-import { Table, Popover } from 'antd';
-import moment from 'moment';
+import React, { useState, useEffect } from 'react'
+import { Table } from '@/lib/mui'
+import { Popover } from 'antd'
 
 const ChunkList = () => {
-    const spaceName = useSearchParams().get('spacename');
-    const documentId = useSearchParams().get('documentid');
-    const [chunkList, setChunkList] = useState<any>([]);
-    useEffect(() => {
-        async function fetchChunks() {
-            const res = await fetch(`http://localhost:8000/knowledge/${spaceName}/chunk/list`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    document_id: documentId
-                }),
-            });
-            const data = await res.json();
-            if (data.success) {
-                setChunkList(data.data);
-            }
+  const spaceName = useSearchParams().get('spacename')
+  const documentId = useSearchParams().get('documentid')
+  const [chunkList, setChunkList] = useState<any>([])
+  useEffect(() => {
+    async function fetchChunks() {
+      const res = await fetch(
+        `http://localhost:8000/knowledge/${spaceName}/chunk/list`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            document_id: documentId
+          })
         }
-        fetchChunks();
-    }, []);
-    return (
-        <div className='p-4'>
-            <Table
-                columns={[
-                    {
-                        title: 'Name',
-                        dataIndex: 'doc_name',
-                        key: 'doc_name',
-                        align: 'center',
-                    },
-                    {
-                        title: 'Content',
-                        dataIndex: 'content',
-                        key: 'content',
-                        align: 'center',
-                        render: (text: string, label: any) => {
-                            return <Popover content={text} trigger="hover">{text.length < 10 ? `${text.slice(0, 10)}...` : text}</Popover>;
-                        }
-                    },
-                    {
-                        title: 'Meta Data',
-                        dataIndex: 'meta_info',
-                        key: 'meta_info',
-                        align: 'center',
-                        render: (text: string, label: any) => {
-                            return <Popover content={JSON.stringify(text || '{}', null, 2)} trigger="hover">{text.length < 10 ? `${text.slice(0, 10)}...` : text}</Popover>;
-                        }
-                    },
-                ]}
-                dataSource={chunkList}
-            />
-        </div>
-    )
+      )
+      const data = await res.json()
+      if (data.success) {
+        setChunkList(data.data)
+      }
+    }
+    fetchChunks()
+  }, [])
+  return (
+    <div className="p-4">
+      <Table sx={{ '& thead th:nth-child(1)': { width: '40%' } }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Content</th>
+            <th>Meta Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {chunkList.map((row: any) => (
+            <tr key={row.id}>
+              <td>{row.doc_name}</td>
+              <td>
+                {
+                  <Popover content={row.content} trigger="hover">
+                    {row.content.length > 10
+                      ? `${row.content.slice(0, 10)}...`
+                      : row.content}
+                  </Popover>
+                }
+              </td>
+              <td>
+                {
+                  <Popover
+                    content={JSON.stringify(row.meta_info || '{}', null, 2)}
+                    trigger="hover"
+                  >
+                    {row.meta_info.length > 10
+                      ? `${row.meta_info.slice(0, 10)}...`
+                      : row.meta_info}
+                  </Popover>
+                }
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  )
 }
 
-export default ChunkList;
+export default ChunkList
