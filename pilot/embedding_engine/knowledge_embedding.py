fa73a4fae61e8b288b4a911fbf6e4f6e974bf997
aries_ckt <916701291@qq.com>
2023-06-27 16:50:16 +0800

    feat: new knowledge chat scene 1.add new knowledge chat scene 2.format file format

--- pilot/embedding_engine/knowledge_embedding.py
+++ pilot/embedding_engine/knowledge_embedding.py
@@ -4,27 +4,10 @@ from chromadb.errors import NotEnoughElementsException
 from langchain.embeddings import HuggingFaceEmbeddings
 
 from pilot.configs.config import Config
-from pilot.embedding_engine.csv_embedding import CSVEmbedding
 from pilot.embedding_engine.knowledge_type import get_knowledge_embedding
-from pilot.embedding_engine.markdown_embedding import MarkdownEmbedding
-from pilot.embedding_engine.pdf_embedding import PDFEmbedding
-from pilot.embedding_engine.ppt_embedding import PPTEmbedding
-from pilot.embedding_engine.url_embedding import URLEmbedding
-from pilot.embedding_engine.word_embedding import WordEmbedding
 from pilot.vector_store.connector import VectorStoreConnector
 
 CFG = Config()
-
-# KnowledgeEmbeddingType = {
-#     ".txt": (MarkdownEmbedding, {}),
-#     ".md": (MarkdownEmbedding, {}),
-#     ".pdf": (PDFEmbedding, {}),
-#     ".doc": (WordEmbedding, {}),
-#     ".docx": (WordEmbedding, {}),
-#     ".csv": (CSVEmbedding, {}),
-#     ".ppt": (PPTEmbedding, {}),
-#     ".pptx": (PPTEmbedding, {}),
-# }
 
 
 class KnowledgeEmbedding:
@@ -57,23 +40,6 @@ class KnowledgeEmbedding:
 
     def init_knowledge_embedding(self):
         return get_knowledge_embedding(self.knowledge_type, self.knowledge_source, self.vector_store_config)
-        # if self.file_type == "url":
-        #     embedding = URLEmbedding(
-        #         file_path=self.file_path,
-        #         vector_store_config=self.vector_store_config,
-        #     )
-        #     return embedding
-        # extension = "." + self.file_path.rsplit(".", 1)[-1]
-        # if extension in KnowledgeEmbeddingType:
-        #     knowledge_class, knowledge_args = KnowledgeEmbeddingType[extension]
-        #     embedding = knowledge_class(
-        #         self.file_path,
-        #         vector_store_config=self.vector_store_config,
-        #         **knowledge_args
-        #     )
-        #     return embedding
-        # raise ValueError(f"Unsupported knowledge file type '{extension}'")
-        # return embedding
 
     def similar_search(self, text, topk):
         vector_client = VectorStoreConnector(
