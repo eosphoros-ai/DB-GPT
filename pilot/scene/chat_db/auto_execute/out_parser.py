8ea2abb29c31124f4f5b0a9398cb9fddd85b081e
tuyang.yhj <tuyang.yhj@alibaba-inc.com>
2023-06-29 17:24:18 +0800

    WEB API independent

--- pilot/scene/chat_db/auto_execute/out_parser.py
+++ pilot/scene/chat_db/auto_execute/out_parser.py
@@ -6,8 +6,9 @@ import pandas as pd
 from pilot.utils import build_logger
 from pilot.out_parser.base import BaseOutputParser, T
 from pilot.configs.model_config import LOGDIR
+from pilot.configs.config import Config
 
-
+CFG = Config()
 class SqlAction(NamedTuple):
     sql: str
     thoughts: Dict
@@ -32,11 +33,16 @@ class DbChatOutputParser(BaseOutputParser):
         if len(data) <= 1:
             data.insert(0, ["result"])
         df = pd.DataFrame(data[1:], columns=data[0])
-        table_style = """<style> 
-            table{border-collapse:collapse;width:100%;height:80%;margin:0 auto;float:center;border: 1px solid #007bff; background-color:#333; color:#fff}th,td{border:1px solid #ddd;padding:3px;text-align:center}th{background-color:#C9C3C7;color: #fff;font-weight: bold;}tr:nth-child(even){background-color:#444}tr:hover{background-color:#444}
-         </style>"""
-        html_table = df.to_html(index=False, escape=False)
-        html = f"<html><head>{table_style}</head><body>{html_table}</body></html>"
+        if CFG.NEW_SERVER_MODE:
+            html = df.to_html(index=False, escape=False, sparsify=False)
+            html = ''.join(html.split())
+        else:
+            table_style = """<style> 
+                table{border-collapse:collapse;width:100%;height:80%;margin:0 auto;float:center;border: 1px solid #007bff; background-color:#333; color:#fff}th,td{border:1px solid #ddd;padding:3px;text-align:center}th{background-color:#C9C3C7;color: #fff;font-weight: bold;}tr:nth-child(even){background-color:#444}tr:hover{background-color:#444}
+             </style>"""
+            html_table = df.to_html(index=False, escape=False)
+            html = f"<html><head>{table_style}</head><body>{html_table}</body></html>"
+
         view_text = f"##### {str(speak)}" + "\n" + html.replace("\n", " ")
         return view_text
 
