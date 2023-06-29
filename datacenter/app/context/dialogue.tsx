f6ea9dc18bc4e721d87c923d0a82ecc7fabce215
changhuiping.chp <changhuiping.chp@digital-engine.com>
2023-06-29 18:52:33 +0800

    feat: dialogue basic development completed

---
+++ datacenter/app/context/dialogue.tsx
@@ -0,0 +1,35 @@
+import { useRequest } from 'ahooks';
+import { sendGetRequest } from '@/utils/request';
+import { createCtx } from '@/utils/ctx-helper';
+import { AxiosResponse } from 'axios';
+import React from 'react';
+
+export const [useDialogueContext, DialogueProvider] = createCtx<{
+	dialogueList?: void | AxiosResponse<any, any> | undefined;
+	queryDialogueList: () => void;
+	refreshDialogList: () => void;
+}>();
+
+export default ({ children }: {
+	children: React.ReactElement
+}) => {
+	const { 
+		run: queryDialogueList,
+		data: dialogueList,
+		refresh: refreshDialogList,
+	} = useRequest(async () => await sendGetRequest('/v1/chat/dialogue/list'), {
+		manual: true,
+	});
+
+	return (
+		<DialogueProvider
+			value={{
+				dialogueList,
+				queryDialogueList,
+				refreshDialogList
+			}}
+		>
+			{children}
+		</DialogueProvider>
+	)
+}
