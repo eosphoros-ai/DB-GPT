4a57192879f6062be7775691f1b91a8bcbfed61e
aries_ckt <916701291@qq.com>
2023-06-29 11:20:16 +0800

    feat:chunks page list 1.add document chunk list

---
+++ pilot/openapi/knowledge/request/knowledge_response.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ChunkQueryResponse(BaseModel):
+    """data: data"""
+    data: List = None
+    """total: total size"""
+    total: int = None
+    """page: current page"""
+    page: int = None
+
+
+class DocumentQueryResponse(BaseModel):
+    """data: data"""
+    data: List = None
+    """total: total size"""
+    total: int = None
+    """page: current page"""
+    page: int = None
+
