b8f3fb38892d82cf81ffd6fca51f0ec9dfbce4fb
changhuiping.chp <changhuiping.chp@digital-engine.com>
2023-07-01 15:50:11 +0800

    fix: change router

--- datacenter/components/agentPage.tsx
+++ datacenter/components/agentPage.tsx
@@ -6,10 +6,8 @@ import ChatBoxComp from '@/components/chatBoxTemp';
 import { useDialogueContext } from '@/app/context/dialogue';
 
 const AgentPage = (props: {
-	params: {
-		agentId?: string;
-	},
 	searchParams: {
+		id?: string;
 		scene?: string;
 		initMessage?: string;
 	}
@@ -17,19 +15,21 @@ const AgentPage = (props: {
 	const { refreshDialogList } = useDialogueContext();
 
 	const { data: historyList } = useRequest(async () => await sendGetRequest('/v1/chat/dialogue/messages/history', {
-		con_uid: props.params?.agentId
+		con_uid: props.searchParams?.id
 	}), {
-		ready: !!props.params?.agentId
+		ready: !!props.searchParams?.id,
+		refreshDeps: [props.searchParams?.id]
 	});
 
 	const { data: paramsList } = useRequest(async () => await sendPostRequest(`/v1/chat/mode/params/list?chat_mode=${props.searchParams?.scene}`), {
-		ready: !!props.searchParams?.scene
+		ready: !!props.searchParams?.scene,
+		refreshDeps: [props.searchParams?.scene]
 	});
 
 	const { history, handleChatSubmit } = useAgentChat({
 		queryAgentURL: `/v1/chat/completions`,
 		queryBody: {
-			conv_uid: props.params?.agentId,
+			conv_uid: props.searchParams?.id,
 			chat_mode: props.searchParams?.scene || 'chat_normal',
 		},
 		initHistory: historyList?.data
