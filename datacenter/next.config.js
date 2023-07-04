bfcf6218ed176d07c9a725075ab26e6fafbbc204
tuyang.yhj <tuyang.yhj@alibaba-inc.com>
2023-07-04 20:09:38 +0800

    WEB API independent

--- datacenter/next.config.js
+++ datacenter/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 		ignoreBuildErrors: true
 	},
 	env: {
-		API_BASE_URL: process.env.API_BASE_URL || 'http://127.0.0.1:5000'
+		API_BASE_URL: process.env.API_BASE_URL'
 	},
 	trailingSlash: true
 }
