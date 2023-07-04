d34e9259ba93f2f2674bcc7eac7e0a1698d4c5f7
tuyang.yhj <tuyang.yhj@alibaba-inc.com>
2023-07-04 20:12:03 +0800

    WEB API independent

---
+++ pilot/server/static/_next/static/esqxweU8ZaWkPpbX1ngPx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-f5357f382422dd96.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
