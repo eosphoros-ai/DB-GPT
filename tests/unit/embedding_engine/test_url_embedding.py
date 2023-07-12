2b68fd7b42769397c26c9d73373f4a8eb6f842d0
aries_ckt <916701291@qq.com>
2023-07-12 11:07:35 +0800

    refactor:refactor knowledge api 1.delete CFG in embedding_engine api 2.add a text_splitter param in embedding_engine api 3.fmt

--- tests/unit/embedding_engine/test_url_embedding.py
+++ tests/unit/embedding_engine/test_url_embedding.py
@@ -5,12 +5,16 @@ embedding_model = "text2vec"
 vector_store_type = "Chroma"
 chroma_persist_path = "your_persist_path"
 vector_store_config = {
-            "vector_store_name": url.replace(":", ""),
-            "vector_store_type": vector_store_type,
-            "chroma_persist_path": chroma_persist_path
-        }
-embedding_engine = EmbeddingEngine(knowledge_source=url, knowledge_type=KnowledgeType.URL.value, model_name=embedding_model, vector_store_config=vector_store_config)
+    "vector_store_name": url.replace(":", ""),
+    "vector_store_type": vector_store_type,
+    "chroma_persist_path": chroma_persist_path,
+}
+embedding_engine = EmbeddingEngine(
+    knowledge_source=url,
+    knowledge_type=KnowledgeType.URL.value,
+    model_name=embedding_model,
+    vector_store_config=vector_store_config,
+)
 
 # embedding url content to vector store
 embedding_engine.knowledge_embedding()
-
