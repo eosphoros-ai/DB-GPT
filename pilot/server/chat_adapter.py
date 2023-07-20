ef2f7999a55f328e99a810b4fc0afa17bbd7f165
FangYin Cheng <staneyffer@gmail.com>
2023-07-20 21:43:20 +0800

    feat: Support llama-2 model

--- pilot/server/chat_adapter.py
+++ pilot/server/chat_adapter.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from functools import cache
-from typing import List
+from typing import List, Dict, Tuple
 from pilot.model.llm_out.vicuna_base_llm import generate_stream
+from pilot.model.conversation import Conversation, get_conv_template
+from pilot.scene.base_message import ModelMessage, ModelMessageRoleType
 
 
 class BaseChatAdpter:
@@ -16,6 +18,52 @@ class BaseChatAdpter:
     def get_generate_stream_func(self):
         """Return the generate stream handler func"""
         pass
+
+    def get_conv_template(self) -> Conversation:
+        return None
+
+    def model_adaptation(self, params: Dict) -> Tuple[Dict, Dict]:
+        """Params adaptation"""
+        conv = self.get_conv_template()
+        messages = params.get("messages")
+        # Some model scontext to dbgpt server
+        model_context = {"prompt_echo_len_char": -1}
+        if not conv or not messages:
+            # Nothing to do
+            return params, model_context
+        conv = conv.copy()
+        system_messages = []
+        for message in messages:
+            role, content = None, None
+            if isinstance(message, ModelMessage):
+                role = message.role
+                content = message.content
+            elif isinstance(message, dict):
+                role = message["role"]
+                content = message["content"]
+            else:
+                raise ValueError(f"Invalid message type: {message}")
+
+            if role == ModelMessageRoleType.SYSTEM:
+                # Support for multiple system messages
+                system_messages.append(content)
+            elif role == ModelMessageRoleType.HUMAN:
+                conv.append_message(conv.roles[0], content)
+            elif role == ModelMessageRoleType.AI:
+                conv.append_message(conv.roles[1], content)
+            else:
+                raise ValueError(f"Unknown role: {role}")
+        if system_messages:
+            conv.update_system_message("".join(system_messages))
+        # Add a blank message for the assistant.
+        conv.append_message(conv.roles[1], None)
+        new_prompt = conv.get_prompt()
+        # Overwrite the original prompt
+        # TODO remote bos token and eos token from tokenizer_config.json of model
+        prompt_echo_len_char = len(new_prompt.replace("</s>", "").replace("<s>", ""))
+        model_context["prompt_echo_len_char"] = prompt_echo_len_char
+        params["prompt"] = new_prompt
+        return params, model_context
 
 
 llm_model_chat_adapters: List[BaseChatAdpter] = []
@@ -134,12 +182,26 @@ class GPT4AllChatAdapter(BaseChatAdpter):
         return gpt4all_generate_stream
 
 
+class Llama2ChatAdapter(BaseChatAdpter):
+    def match(self, model_path: str):
+        return "llama-2" in model_path.lower()
+
+    def get_conv_template(self) -> Conversation:
+        return get_conv_template("llama-2")
+
+    def get_generate_stream_func(self):
+        from pilot.model.inference import generate_stream
+
+        return generate_stream
+
+
 register_llm_model_chat_adapter(VicunaChatAdapter)
 register_llm_model_chat_adapter(ChatGLMChatAdapter)
 register_llm_model_chat_adapter(GuanacoChatAdapter)
 register_llm_model_chat_adapter(FalconChatAdapter)
 register_llm_model_chat_adapter(GorillaChatAdapter)
 register_llm_model_chat_adapter(GPT4AllChatAdapter)
+register_llm_model_chat_adapter(Llama2ChatAdapter)
 
 # Proxy model for test and develop, it's cheap for us now.
 register_llm_model_chat_adapter(ProxyllmChatAdapter)
