0f220f71fc01e59d35960c17f0dca49c840f6d72
yhjun1026 <tuyang.yhj@alibaba-inc.com>
2023-07-21 17:25:16 +0800

    Multi DB support

---
+++ pilot/connections/db_conn_info.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+class DBConfig(BaseModel):
+    db_type: str
+    db_name: str
+    file_path: str = ""
+    db_host: str = ""
+    db_port: int = 0
+    db_user: str = ""
+    db_pwd: str = ""
+    comment: str = ""
