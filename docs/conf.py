8657cf64d57c8004056cf4ee0b6c30486357323c
csunny <cfqsunny@163.com>
2023-07-24 19:45:56 +0800

    chores: version + rm useless file

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ project = "DB-GPT"
 copyright = "2023, csunny"
 author = "csunny"
 
-version = "👏👏 0.3.0"
+version = "👏👏 0.3.2"
 html_title = project + " " + version
 
 # -- General configuration ---------------------------------------------------
