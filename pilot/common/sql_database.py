0d310710e1f042f8c6bbbe94df85ed3b8fdcf315
csunny <cfqsunny@163.com>
2023-07-24 19:27:24 +0800

    chroes: fix + lint

--- pilot/common/sql_database.py
+++ pilot/common/sql_database.py
@@ -345,7 +345,14 @@ class Database:
         return [
             d[0]
             for d in results
-            if d[0] not in ["information_schema", "performance_schema", "sys", "mysql", "knowledge_management"]
+            if d[0]
+            not in [
+                "information_schema",
+                "performance_schema",
+                "sys",
+                "mysql",
+                "knowledge_management",
+            ]
         ]
 
     def convert_sql_write_to_select(self, write_sql):
