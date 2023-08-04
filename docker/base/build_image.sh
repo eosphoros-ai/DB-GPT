b16da50441cb41aecdfa3c97c2277c5663723c44
guochaohui <526420619@qq.com>
2023-08-04 11:04:58 +0800

    fix bug with issues/410: build docker image use param not working

--- docker/base/build_image.sh
+++ docker/base/build_image.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+###
+ # @Author: guochaohui 526420619@qq.com
+ # @Date: 2023-08-04 11:02:02
+ # @LastEditors: guochaohui 526420619@qq.com
+ # @LastEditTime: 2023-08-04 11:02:02
+ # @FilePath: /DB-GPT/docker/base/build_image.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 
 SCRIPT_LOCATION=$0
 cd "$(dirname "$SCRIPT_LOCATION")"
@@ -36,7 +44,7 @@ while [[ $# -gt 0 ]]; do
         shift # past value
         ;;
         -i|--pip-index-url)
-        PIP_INDEX="$2"
+        PIP_INDEX_URL="$2"
         shift
         shift
         ;;
