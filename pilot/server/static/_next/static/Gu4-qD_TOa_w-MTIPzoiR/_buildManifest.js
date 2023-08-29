c3eacd747cdfba484eec175ce9c156206545185a
yhjun1026 <460342015@qq.com>
2023-08-29 15:12:21 +0800

    feat(editor): ChatExcel
    🔥ChatExcel Mode Complete

--- pilot/server/static/_next/static/Gu4-qD_TOa_w-MTIPzoiR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-bcb3296e330590f7.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
