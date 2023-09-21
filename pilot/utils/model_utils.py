28c24516a2538554b9d432352c10393e98288af0
FangYin Cheng <staneyffer@gmail.com>
2023-09-21 18:19:19 +0800

    chore: Add _clear_model_cache function to clear model cache

--- pilot/utils/model_utils.py
+++ pilot/utils/model_utils.py
@@ -1,11 +1,21 @@
 import logging
 
+logger = logging.getLogger(__name__)
+
+
+def _clear_model_cache(device="cuda"):
+    try:
+        # clear torch cache
+        import torch
+
+        _clear_torch_cache(device)
+    except ImportError:
+        logger.warn("Torch not installed, skip clear torch cache")
+    # TODO clear other cache
+
 
 def _clear_torch_cache(device="cuda"):
-    try:
-        import torch
-    except ImportError:
-        return
+    import torch
     import gc
 
     gc.collect()
@@ -16,14 +26,14 @@ def _clear_torch_cache(device="cuda"):
 
                 empty_cache()
             except Exception as e:
-                logging.warn(f"Clear mps torch cache error, {str(e)}")
+                logger.warn(f"Clear mps torch cache error, {str(e)}")
         elif torch.has_cuda:
             device_count = torch.cuda.device_count()
             for device_id in range(device_count):
                 cuda_device = f"cuda:{device_id}"
-                logging.info(f"Clear torch cache of device: {cuda_device}")
+                logger.info(f"Clear torch cache of device: {cuda_device}")
                 with torch.cuda.device(cuda_device):
                     torch.cuda.empty_cache()
                     torch.cuda.ipc_collect()
         else:
-            logging.info("No cuda or mps, not support clear torch cache yet")
+            logger.info("No cuda or mps, not support clear torch cache yet")
