74a3f6edb53854a8d5a6ccf7dc73de0367d1f3a8
yhjun1026 <460342015@qq.com>
2023-11-10 11:36:57 +0800

    bugfix(ChatData): ChatData Use AntV Table
    1.Merge ChatData and ChatDB

--- pilot/scene/chat_db/auto_execute/out_parser.py
+++ pilot/scene/chat_db/auto_execute/out_parser.py
@@ -37,6 +37,7 @@ class DbChatOutputParser(BaseOutputParser):
 
         param = {}
         api_call_element = ET.Element("chart-view")
+        err_msg = None
         try:
             df = data(prompt_response.sql)
             param["type"] = "response_table"
@@ -44,17 +45,18 @@ class DbChatOutputParser(BaseOutputParser):
             param["data"] = json.loads(df.to_json(orient='records', date_format='iso', date_unit='s'))
             view_json_str = json.dumps(param, default=serialize)
         except Exception as e:
-            err_param ={}
-            param["sql"] = prompt_response.sql
+            logger.error("parse_view_response error!" + str(e))
+            err_param = {}
+            err_param["sql"] = f'{prompt_response.sql}'
             err_param["type"] = "response_table"
             err_param["err_msg"] = str(e)
+            err_param["data"] = []
+            err_msg = str(e)
             view_json_str = json.dumps(err_param, default=serialize)
 
         api_call_element.text = view_json_str
         result = ET.tostring(api_call_element, encoding="utf-8")
-
-        return speak + "\n" + result.decode("utf-8")
-
-
-
-
+        if err_msg:
+            return f"""{speak} \\n <span style=\"color:red\">ERROR!</span>{err_msg} \n {result.decode("utf-8")}"""
+        else:
+            return speak + "\n" + result.decode("utf-8")
