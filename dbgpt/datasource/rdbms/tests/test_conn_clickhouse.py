dfc708665f56212e8877d8d6b6950f64cb82324d
csunny <cfqsunny@163.com>
2023-12-18 16:39:24 +0800

    tests: add testcase for clickhouse

---
+++ dbgpt/datasource/rdbms/tests/test_conn_clickhouse.py
@@ -0,0 +1,8 @@
+import pytest
+
+from dbgpt.datasource.rdbms.conn_clickhouse import ClickhouseConnect
+
+@pytest.fixture
+def db():
+    conn = ClickhouseConnect.from_uri_db("localhost", 8123, "default", "", "default")
+    yield conn
