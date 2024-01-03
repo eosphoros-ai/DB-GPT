2279de71dbf67f242c09652d917827d7baf271f3
aries_ckt <916701291@qq.com>
2024-01-03 23:08:32 +0800

    feat:add chunk strategy parameter description

--- dbgpt/rag/knowledge/base.py
+++ dbgpt/rag/knowledge/base.py
@@ -47,8 +47,18 @@ class ChunkStrategy(Enum):
     CHUNK_BY_SIZE = (
         RecursiveCharacterTextSplitter,
         [
-            {"param_name": "chunk_size", "param_type": "int", "default_value": 512},
-            {"param_name": "chunk_overlap", "param_type": "int", "default_value": 50},
+            {
+                "param_name": "chunk_size",
+                "param_type": "int",
+                "default_value": 512,
+                "description": "The size of the data chunks used in processing.",
+            },
+            {
+                "param_name": "chunk_overlap",
+                "param_type": "int",
+                "default_value": 50,
+                "description": "The amount of overlap between adjacent data chunks.",
+            },
         ],
         "chunk size",
         "split document by chunk size",
@@ -56,18 +66,31 @@ class ChunkStrategy(Enum):
     CHUNK_BY_PAGE = (PageTextSplitter, [], "page", "split document by page")
     CHUNK_BY_PARAGRAPH = (
         ParagraphTextSplitter,
-        [{"param_name": "separator", "param_type": "string", "default_value": "\n"}],
+        [
+            {
+                "param_name": "separator",
+                "param_type": "string",
+                "default_value": "\\n",
+                "description": "paragraph separator",
+            }
+        ],
         "paragraph",
         "split document by paragraph",
     )
     CHUNK_BY_SEPARATOR = (
         SeparatorTextSplitter,
         [
-            {"param_name": "separator", "param_type": "string", "default_value": "\n"},
+            {
+                "param_name": "separator",
+                "param_type": "string",
+                "default_value": "\\n",
+                "description": "chunk separator",
+            },
             {
                 "param_name": "enable_merge",
                 "param_type": "boolean",
                 "default_value": False,
+                "description": "Whether to merge according to the chunk_size after splitting by the separator.",
             },
         ],
         "separator",
