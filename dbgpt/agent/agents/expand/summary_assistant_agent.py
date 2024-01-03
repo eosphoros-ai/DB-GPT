d774286504a6b3c54428825a606d9363d885a318
qidanrui <qidanrui@gmail.com>
2024-01-03 10:16:51 +0000

    feat(multi-agents):Add Summary Assistant Agent

---
+++ dbgpt/agent/agents/expand/summary_assistant_agent.py
@@ -0,0 +1,96 @@
+from typing import Callable, Dict, Literal, Optional, Union
+
+from dbgpt._private.config import Config
+from dbgpt.agent.agents.base_agent import ConversableAgent
+from dbgpt.agent.plugin.commands.command_mange import ApiCall
+
+from ...memory.gpts_memory import GptsMemory
+from ..agent import Agent, AgentContext
+
+
+class SummaryAssistantAgent(ConversableAgent):
+    """(In preview) Assistant agent, designed to solve a task with LLM.
+
+    AssistantAgent is a subclass of ConversableAgent configured with a default system message.
+    The default system message is designed to solve a task with LLM,
+    including suggesting python code blocks and debugging.
+    `human_input_mode` is default to "NEVER"
+    and `code_execution_config` is default to False.
+    This agent doesn't execute code by default, and expects the user to execute the code.
+    """
+
+    DEFAULT_SYSTEM_MESSAGE = (
+        """You are a great summary writter to summarize the provided text content."""
+    )
+
+    DEFAULT_DESCRIBE = (
+        """Summarize provided text content and output the summaraization."""
+    )
+
+    NAME = "Summarizer"
+
+    def __init__(
+        self,
+        memory: GptsMemory,
+        agent_context: AgentContext,
+        describe: Optional[str] = DEFAULT_DESCRIBE,
+        is_termination_msg: Optional[Callable[[Dict], bool]] = None,
+        max_consecutive_auto_reply: Optional[int] = None,
+        human_input_mode: Optional[str] = "NEVER",
+        **kwargs,
+    ):
+        super().__init__(
+            name=self.NAME,
+            memory=memory,
+            describe=describe,
+            system_message=self.DEFAULT_SYSTEM_MESSAGE,
+            is_termination_msg=is_termination_msg,
+            max_consecutive_auto_reply=max_consecutive_auto_reply,
+            human_input_mode=human_input_mode,
+            agent_context=agent_context,
+            **kwargs,
+        )
+        self.register_reply(Agent, SummaryAssistantAgent.generate_summary_reply)
+        self.agent_context = agent_context
+
+    async def generate_summary_reply(
+        self,
+        message: Optional[str] = None,
+        sender: Optional[Agent] = None,
+        reviewer: Optional[Agent] = None,
+        config: Optional[Union[Dict, Literal[False]]] = None,
+    ):
+        """Generate a reply with summary."""
+
+        # if not message is None:
+        #     exit_success = True
+        #     output = "The generated summary is shown above."
+        # else:
+        #     exit_success = False
+        #     output = "Nothing is generated, please check your input."
+
+        # return True, {"is_exe_success": exit_success, "content": f"{output}"}
+
+        response_success = True
+        view = None
+        content = None
+        if message is None:
+            # Answer failed, turn on automatic repair
+            fail_reason += f"Nothing is summarized, please check your input."
+            response_success = False
+        else:
+            try:
+                vis_client = ApiCall()
+                view = vis_client.display_summary_vis(message)
+                content = "The generated summary is shown above."
+            except Exception as e:
+                fail_reason += f"Return summarization error，{str(e)}"
+                response_success = False
+
+        if not response_success:
+            content = fail_reason
+        return True, {
+            "is_exe_success": response_success,
+            "content": content,
+            "view": view,
+        }
