5bc419679b2ed89d5044e3cb211e7adf9be931ba
Aries-ckt <916701291@qq.com>
2024-01-05 16:37:33 +0800

    docs: update chat_knowledge.md and wechat.jpg (#1035)

--- setup.py
+++ setup.py
@@ -638,7 +638,7 @@ init_install_requires()
 setuptools.setup(
     name="db-gpt",
     packages=find_packages(exclude=("tests", "*.tests", "*.tests.*", "examples")),
-    version="0.4.4",
+    version="0.4.5",
     author="csunny",
     author_email="cfqcsunny@gmail.com",
     description="DB-GPT is an experimental open-source project that uses localized GPT large models to interact with your data and environment."
