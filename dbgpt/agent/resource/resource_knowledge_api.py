75d40fe230712bddd69e8f9f67395795197972e3
yhjun1026 <460342015@qq.com>
2024-02-05 18:18:38 +0800

    feat(agent):Awel Flow + Agent 1.Add Agent Operator's Resource Define

--- dbgpt/agent/resource/resource_knowledge_api.py
+++ dbgpt/agent/resource/resource_knowledge_api.py
@@ -1,20 +1,19 @@
 from typing import Any, Dict, List, Optional, Tuple, Union
 
+from dbgpt.rag.retriever.base import BaseRetriever
+
 from .resource_api import ResourceClient, ResourceType
 
 
-class ResourceDbClient(ResourceClient):
+class ResourceKnowledgeClient(ResourceClient):
     @property
     def type(self):
         return ResourceType.Knowledge
 
-    async def a_get_schema_link(
-        self, db_name: str, question: Optional[str] = None
-    ) -> str:
+    async def a_get_kn(self, space_name: str, question: Optional[str] = None) -> str:
         raise NotImplementedError("The run method should be implemented in a subclass.")
 
-    async def a_query_to_df(self, db_name: str, sql: str):
-        raise NotImplementedError("The run method should be implemented in a subclass.")
-
-    async def a_run_sql(self, db_name: str, sql: str):
+    async def add_kn(
+        self, space_name: str, kn_name: str, type: str, content: Optional[Any]
+    ):
         raise NotImplementedError("The run method should be implemented in a subclass.")
