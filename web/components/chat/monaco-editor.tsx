04322de4f3d866681bc74009a17aee8804699079
Hzh_97 <37397994+2976151305@users.noreply.github.com>
2024-03-21 17:41:25 +0800

    fix(web): Fix sql format error (#1319)

--- web/components/chat/monaco-editor.tsx
+++ web/components/chat/monaco-editor.tsx
@@ -2,7 +2,7 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
 import Editor, { OnChange, loader } from '@monaco-editor/react';
 import classNames from 'classnames';
 import { useMemo } from 'react';
-import { format } from 'sql-formatter';
+import { formatSql } from '@/utils';
 
 loader.config({ monaco });
 
@@ -21,9 +21,9 @@ export default function MonacoEditor({ className, value, language = 'mysql', onC
       return value;
     }
     if (thoughts && thoughts.length > 0) {
-      return format(`-- ${thoughts} \n${value}`);
+      return formatSql(`-- ${thoughts} \n${value}`);
     }
-    return format(value);
+    return formatSql(value);
   }, [value, thoughts]);
 
   return (
