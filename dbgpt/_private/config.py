4238a670bf899a084c93fb512429f359a095feb7
付 <hi_php@163.com>
2024-03-29 17:25:42 +0800

    fix: Fix the issue of invalid port settings (#1351)

--- dbgpt/_private/config.py
+++ dbgpt/_private/config.py
@@ -25,7 +25,7 @@ class Config(metaclass=Singleton):
 
         # Gradio language version: en, zh
         self.LANGUAGE = os.getenv("LANGUAGE", "en")
-        self.WEB_SERVER_PORT = int(os.getenv("WEB_SERVER_PORT", 7860))
+        self.WEB_SERVER_PORT = int(os.getenv("WEB_SERVER_PORT", 5000))
 
         self.debug_mode = False
         self.skip_reprompt = False
