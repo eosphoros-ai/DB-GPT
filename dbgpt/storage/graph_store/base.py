771cdd3df858261ad563ef6a042296892b8c566b
KingSkyLi <15566300566@163.com>
2024-04-26 11:29:57 +0800

    add graph store

--- dbgpt/storage/graph_store/base.py
+++ dbgpt/storage/graph_store/base.py
@@ -1,7 +1,7 @@
 """Graph store base class."""
 import logging
 from abc import ABC
-
+from typing import Any,List,Optional,Dict
 from dbgpt._private.pydantic import BaseModel
 
 logger = logging.getLogger(__name__)
@@ -13,3 +13,25 @@ class GraphStoreConfig(BaseModel):
 
 class GraphStoreBase(ABC):
     """Graph store base class."""
+    @property
+    def client(self) -> Any:
+        """Get client."""
+
+    def get_triplets(self, subj: str) -> List[List[str]]:
+        """Get triplets."""
+
+    def insert_triplet(self, subj: str, rel: str, obj: str) -> None:
+        """Add triplet."""
+        ...
+
+    def delete_triplets(self, subj: str, rel: str, obj: str) -> None:
+        """Delete triplet."""
+        ...
+
+    def get_schema(self, refresh: bool = False) -> str:
+        """Get the schema of the graph store."""
+        ...
+
+    def query(self, query: str, param_map: Optional[Dict[str, Any]] = {}) -> Any:
+        """Query the graph store with statement and parameters."""
+        ...
--- dbgpt/storage/graph_store/tugraph_store.py
+++ dbgpt/storage/graph_store/tugraph_store.py
@@ -1,10 +1,28 @@
 """TuGraph vector store."""
 import logging
-
-from .base import GraphStoreBase
-
+from dbgpt.storage.graph_store.base import GraphStoreBase
+from dbgpt.datasource.conn_tugraph import TuGraphConnector
+from typing import Any,Optional
 logger = logging.getLogger(__name__)
+
 
 
 class TuGraphStore(GraphStoreBase):
     """TuGraph vector store."""
+
+    def __init__(self, host: str, port: int, user: str, pwd: str, db_name: str, node_label: str = 'entity', edge_label: str = 'rel', **kwargs: Any) -> None:
+        """Initialize the TuGraphStore with connection details."""
+        self.conn = TuGraphConnector.from_uri_db(host=host, port=port, user=user, pwd=pwd, db_name=db_name)
+        self._node_label = node_label
+        self._edge_label = edge_label
+        self._create_schema()
+
+    def _create_schema(self):
+        create_vertex_gql = f"CALL db.createLabel('vertex', '{self._node_label}', 'id', ['id',string,false])"
+        create_edge_gql = f"CALL db.createLabel('edge', '{self._edge_label}', '[[\"{self._node_label}\",\"{self._node_label}\"]]', ['id',string,false]);"
+        self.conn.run(create_vertex_gql)
+        self.conn.run(create_edge_gql)
+
+    
+        
+
