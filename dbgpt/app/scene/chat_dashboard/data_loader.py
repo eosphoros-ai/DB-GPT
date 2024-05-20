819f743698e51cdbde62ce9f189f88fdae8092d3
yongzheJIN <52804241+yongzheJIN@users.noreply.github.com>
2024-05-20 12:00:30 +0800

    format the data_loader.py/dashboard
    use make fmt to format data_loaderpy

--- dbgpt/app/scene/chat_dashboard/data_loader.py
+++ dbgpt/app/scene/chat_dashboard/data_loader.py
@@ -19,18 +19,30 @@ class DashboardDataLoader:
 
     def get_chart_values_by_data(self, field_names, datas, chart_sql: str):
         logger.info(f"get_chart_values_by_conn:{chart_sql}")
+        # try:
         values: List[ValueItem] = []
         data_map = {}
 
         data_map.update(
-            {f"{field_name}": [row[index] for row in datas] for index, field_name in enumerate(field_names)})
+            {
+                f"{field_name}": [row[index] for row in datas]
+                for index, field_name in enumerate(field_names)
+            }
+        )
         # to Check Whether there are data in it
         if len(datas) != 0:
             # find the first string column
-            str_index = next((index for index, value in enumerate(datas[0]) if isinstance(value, str)), 1)
+            str_index = next(
+                (
+                    index
+                    for index, value in enumerate(datas[0])
+                    if isinstance(value, str)
+                ),
+                1,
+            )
             if type(datas[0][str_index]) == str:
                 tempFieldName = field_names[:str_index]
-                tempFieldName.extend(field_names[str_index + 1:])
+                tempFieldName.extend(field_names[str_index + 1 :])
                 for field_name in tempFieldName:
                     for data in datas:
                         # None Data won't be ok for the chart
@@ -59,8 +71,10 @@ class DashboardDataLoader:
                     values.append(value_item)
             return field_names, values
         else:
-            return field_names, [ValueItem(name=f'{field_name}', type=f"{field_name}", value='0') for index, field_name
-                                 in enumerate(field_names)]
+            return field_names, [
+                ValueItem(name=f"{field_name}", type=f"{field_name}", value="0")
+                for index, field_name in enumerate(field_names)
+            ]
 
     def get_chart_values_by_db(self, db_name: str, chart_sql: str):
         logger.info(f"get_chart_values_by_db:{db_name},{chart_sql}")
