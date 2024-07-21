731b8091d896e28959fbdfce543f623c0d897b92
Fangyin Cheng <staneyffer@gmail.com>
2024-07-22 02:12:10 +0800

    feat(docs): Build multi-version document images

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -53,8 +53,8 @@ const config = {
   organizationName: 'eosphoros-ai', // Usually your GitHub org/user name.
   projectName: 'DB-GPT', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: isDev ? 'throw' : 'warn',
+  onBrokenMarkdownLinks: isDev ? 'throw' : 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
