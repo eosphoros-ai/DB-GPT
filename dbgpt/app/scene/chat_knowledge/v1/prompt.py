04e01bc2af1401be24cf97bde3c30c8d1d5b9773
aries_ckt <916701291@qq.com>
2024-07-23 13:31:40 +0800

    feat:change prompt

--- dbgpt/app/scene/chat_knowledge/v1/prompt.py
+++ dbgpt/app/scene/chat_knowledge/v1/prompt.py
@@ -18,7 +18,7 @@ _DEFAULT_TEMPLATE_ZH = """ 基于以下给出的已知信息, 准守规范约束
 规范约束:
      1.如果已知信息包含的图片、链接、表格、代码块等特殊markdown标签格式的信息，确保在答案中包含原文这些图片、链接、表格和代码标签，不要丢弃不要修改，如:图片格式：![image.png](xxx), 链接格式:[xxx](xxx), 表格格式:|xxx|xxx|xxx|, 代码格式:```xxx```.
      2.如果无法从提供的内容中获取答案, 请说: "知识库中提供的内容不足以回答此问题" 禁止胡乱编造.
-     3.回答的时候最好按照1.2.3.点进行总结.
+     3.回答的时候最好按照1.2.3.点进行总结, 并以markdwon格式显示.
             已知内容: 
             {context}
             问题:
@@ -29,7 +29,7 @@ constraints:
     1.Ensure to include original markdown formatting elements such as images, links, tables, or code blocks without alteration in the response if they are present in the provided information.
         For example, image format should be ![image.png](xxx), link format [xxx](xxx), table format should be represented with |xxx|xxx|xxx|, and code format with xxx.
     2.If the information available in the knowledge base is insufficient to answer the question, state clearly: "The content provided in the knowledge base is not enough to answer this question," and avoid making up answers.
-    3.When responding, it is best to summarize the points in the order of 1, 2, 3.
+    3.When responding, it is best to summarize the points in the order of 1, 2, 3, And displayed in markdwon format.
             known information: 
             {context}
             question:
