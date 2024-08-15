b264325151543cb1ba25156155163462b7ac5ac4
严志勇 <yanzhiyong@tiansuixiansheng.com>
2024-08-15 18:24:53 +0800

    feat:code editor

--- web/components/flow/node-renderer/codeEditor.tsx
+++ web/components/flow/node-renderer/codeEditor.tsx
@@ -1,28 +1,97 @@
-import React, { useState } from 'react';  
-import { UnControlled as CodeMirror } from 'react-codemirror2';  
+import React, { useState, useMemo } from 'react';
+import { Button, Modal } from 'antd';
+import Editor from '@monaco-editor/react';
+import { IFlowNodeParameter } from '@/types/flow';
+// import { MonacoEditor } from '../../chat/monaco-editor';
+// import { github, githubDark } from './ob-editor/theme';
+import { github, githubDark } from '../../chat/ob-editor/theme';
 
-  
-export const RenderCodeEditor = ()=> {  
-  const [code, setCode] = useState('// 输入你的代码');  
-  
-  const handleChange = (editor, data, value) => {  
-    // 处理代码变化  
-    setCode(value);  
-  };  
-  
-  return (  
-    <CodeMirror  
-      value={code}  
-      onChange={handleChange}  
-      options={{  
-        mode: 'javascript', // 设置语言模式  
-        theme: 'material', // 设置主题  
-        lineNumbers: true, // 显示行号  
-        // 其他配置...  
-      }}  
-    />  
-  );  
-}  
-  
+type Props = {
+  data: IFlowNodeParameter;
+  defaultValue: any;
+  onChange: (value: any) => void;
+};
 
+export const RenderCodeEditor = (params: Props) => {
+  const { data, defaultValue, onChange } = params;
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  /**
+   * 设置弹窗宽度
+   */
+  const modalWidth = useMemo(() => {
+    if (data?.ui?.editor?.width) {
+      return data?.ui?.editor?.width + 100
+    }
+    return '80%';
+  }, [data?.ui?.editor?.width]);
+
+  return (
+    <div style={{textAlign:'center'}} className="p-2 text-sm">
+      <Button type="primary" onClick={showModal}>
+        打开代码编辑器
+      </Button>
+      <Modal title="代码编辑:" width={modalWidth} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Editor
+          {...data?.ui?.attr}
+          width={data?.ui?.editor?.width || '100%'}
+          value={defaultValue}
+          style={{padding:'10px'}}
+          height={data?.ui?.editor?.height || 200}
+          defaultLanguage={data?.ui?.language}
+          onChange={onChange}
+          theme='vs-dark' // 编辑器主题颜色
+          options={{
+            minimap: {
+              enabled: false,
+            },
+            wordWrap: 'on',
+          }}
+        />
+
+        {/* <Editor
+          {...data?.ui?.attr}
+          language={data?.ui?.language}
+          width={data?.ui?.editor?.width || '100%'}
+          value={defaultValue}
+          height={data?.ui?.editor?.height || 200}
+          defaultValue={defaultValue}
+          onChange={(value: string | undefined) => {
+            console.log(value);
+            onChange(value)
+          }}
+          options={{
+            theme: {github}, // 编辑器主题颜色
+            folding: true, // 是否折叠
+            foldingHighlight: true, // 折叠等高线
+            foldingStrategy: 'indentation', // 折叠方式  auto | indentation
+            showFoldingControls: 'always', // 是否一直显示折叠 always | mouseover
+            disableLayerHinting: true, // 等宽优化
+            emptySelectionClipboard: false, // 空选择剪切板
+            selectionClipboard: false, // 选择剪切板
+            automaticLayout: true, // 自动布局
+            codeLens: false, // 代码镜头
+            scrollBeyondLastLine: false, // 滚动完最后一行后再滚动一屏幕
+            colorDecorators: true, // 颜色装饰器
+            accessibilitySupport: 'auto', // 辅助功能支持  "auto" | "off" | "on"
+            lineNumbers: 'on', // 行号 取值： "on" | "off" | "relative" | "interval" | function
+            lineNumbersMinChars: 5, // 行号最小字符   number
+            readOnly: false, //是否只读  取值 true | false
+          }}
+        /> */}
+      </Modal>
+
+    </div>
+  );
+};
