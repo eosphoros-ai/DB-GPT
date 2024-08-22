e7d81a2db10d24e06925f71a57721b9bc4d79173
yanzhiyong <932374019@qq.com>
2024-08-23 02:56:05 +0800

    feat: slider range

--- web/components/flow/node-renderer/slider.tsx
+++ web/components/flow/node-renderer/slider.tsx
@@ -6,5 +6,9 @@ import { Slider } from 'antd';
 export const renderSlider = (data: IFlowNodeParameter) => {
   const attr = convertKeysToCamelCase(data.ui?.attr || {});
 
-  return <Slider className="mt-8 nodrag" {...attr} tooltip={{ open: true }} />;
+  return (
+    <>
+    {data.is_list?(<Slider range className="mt-8 nodrag" {...attr} tooltip={{ open: true }} />):<Slider className="mt-8 nodrag" {...attr} tooltip={{ open: true }} />;}
+    </>
+  )
 };
