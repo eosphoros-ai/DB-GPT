f6b9dfa8c2482602ac3a24868719865bd68a602f
云锋 <932374019@qq.com>
2024-09-09 11:40:05 +0800

    Update flow.ts

--- web/locales/zh/flow.ts
+++ web/locales/zh/flow.ts
@@ -22,9 +22,8 @@ export const FlowZn = {
   Higher_Order_Nodes: '高阶',
   All_Nodes: '所有',
   Import_From_Template: '导入模版 ',
-  Import_Template: '从模版导入',
-  Template_Name: '模版名称',
   Template_Description: '描述',
+  Template_Name: '模版名称',
   Template_Label: '标签',
   Template_Action: '操作',
 };
