55e21538989e9b5feed8f32cee2474f8ff6f311c
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-15 16:04:52 +0800

    feat(server): update csp for embedding the libro iframe

--- dbgpt/serve/libro/service/jupyter_server_config.py
+++ dbgpt/serve/libro/service/jupyter_server_config.py
@@ -2,7 +2,7 @@ c = get_config()  # noqa
 # 为了 dbgpt 页面能 iframe 能正确加载显示 libro 页面
 c.ServerApp.tornado_settings = {
     "headers": {
-        "Content-Security-Policy": "frame-ancestors 'self' http://localhost:3001"
+        "Content-Security-Policy": "frame-ancestors 'self' *"
     }
 }
 # 默认启动 libro 不自动打开新的页面
