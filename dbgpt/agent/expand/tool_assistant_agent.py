25b8bd193f9a56f719def2f09b7769cf1c781eea
cinjospeh <joseph.cjn@alibaba-inc.com>
2025-01-10 09:47:49 +0800

    fix format problem

--- dbgpt/agent/expand/tool_assistant_agent.py
+++ dbgpt/agent/expand/tool_assistant_agent.py
@@ -91,6 +91,8 @@ def _get_tools_by_resource(resource: Optional[Resource]) -> Optional[List[BaseTo
         tools.append(resource)
     elif resource.type() == ResourceType.Pack:
         for sub_res in resource.sub_resources:
-            tools.extend(_get_tools_by_resource(sub_res))
+            res_list = _get_tools_by_resource(sub_res)
+            if res_list is not None and len(res_list) > 0:
+                tools.extend(_get_tools_by_resource(sub_res))
 
     return tools
