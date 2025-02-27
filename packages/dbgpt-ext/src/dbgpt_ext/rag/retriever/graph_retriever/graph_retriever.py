faa82dc6e014a35777a2dc47aaa9cb9b1c032b45
SonglinLyu <111941624+SonglinLyu@users.noreply.github.com>
2025-02-27 21:56:19 +0800

    GraphRAG: add new feature and bugfix (#2373)
    Co-authored-by: 秉翟 <lyusonglin.lsl@antgroup.com>

--- packages/dbgpt-ext/src/dbgpt_ext/rag/retriever/graph_retriever/graph_retriever.py
+++ packages/dbgpt-ext/src/dbgpt_ext/rag/retriever/graph_retriever/graph_retriever.py
@@ -94,6 +94,15 @@ class GraphRetriever(GraphRetrieverBase):
             "TEXT2GQL_MODEL_NAME",
             config.text2gql_model_name,
         )
+        text2gql_model_enabled = (
+            os.environ["TEXT2GQL_MODEL_ENABLED"].lower() == "true"
+            if "TEXT2GQL_MODEL_ENABLED" in os.environ
+            else config.text2gql_model_enabled
+        )
+        text2gql_model_name = os.getenv(
+            "TEXT2GQL_MODEL_NAME",
+            config.text2gql_model_name,
+        )
 
         self._keyword_extractor = KeywordExtractor(llm_client, model_name)
         self._text_embedder = TextEmbedder(config.embedding_fn)
