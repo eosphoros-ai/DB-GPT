5199b53529c46fa0103e9d9166367b6f38a01cff
Fangyin Cheng <staneyffer@gmail.com>
2025-03-07 12:48:03 +0800

    chore: Fix build web error

--- packages/dbgpt-app/src/dbgpt_app/static/web/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/construct/app":["static\u002Fchunks\u002Fpages\u002Fconstruct\u002Fapp.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fconstruct\u002Fapp"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
