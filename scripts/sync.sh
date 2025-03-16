748255d55a219c27914e0090068ed13320ca3f26
tam <tanwe@fulan.com.cn>
2025-03-16 21:44:46 +0800

    Update dependencies and configuration files
    - Added `dbgpt-docs` and `db-gpt-web` as local file dependencies in `package.json` and `package-lock.json`.
    - Updated `.gitignore` to include new configuration files.
    - Modified `uv.lock` to include additional resolution markers and updated dependency versions.
    - Updated `yarn.lock` with new package versions and dependencies for better compatibility.

---
+++ scripts/sync.sh
@@ -0,0 +1,10 @@
+uv sync --all-packages \
+--extra "base" \
+--extra "proxy_openai" \
+--extra "rag" \
+--extra "storage_chromadb" \
+--extra "dbgpts" \
+--extra "quant_bnb" \
+--extra "datasource_hive" \
+--extra "datasource_mssql" \
+--index-url=https://pypi.tuna.tsinghua.edu.cn/simple
