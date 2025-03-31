b91fb80af152f00c72dc926410cfbc6671c5cb87
yyhhyyyyyy <95077259+yyhhyyyyyy@users.noreply.github.com>
2025-03-31 12:16:05 +0800

    fix(web): Prevent dragging node when selecting text in parameter input fields

--- web/components/flow/node-renderer/textarea.tsx
+++ web/components/flow/node-renderer/textarea.tsx
@@ -6,5 +6,5 @@ const { TextArea } = Input;
 
 export const renderTextArea = (data: IFlowNodeParameter) => {
   const attr = convertKeysToCamelCase(data.ui?.attr || {});
-  return <TextArea className='nowheel mb-3' {...attr} />;
+  return <TextArea className='nowheel mb-3 nodrag' {...attr} />;
 };
