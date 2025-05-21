a51c26e472a5491577745a7d3cbcaa1ea0fa61fc
geebytes <vforfreedom96@gmail.com>
2025-05-21 02:41:59 +0000

    feat(dev):add dev container support zh_CN

--- packages/dbgpt-core/src/dbgpt/storage/base.py
+++ packages/dbgpt-core/src/dbgpt/storage/base.py
@@ -129,7 +129,7 @@ class IndexStoreBase(ABC):
         max_threads = max_threads or self._max_threads
         # Group the chunks into chunks of size max_chunks
         chunk_groups = [
-            chunks[i: i + max_chunks_once_load]
+            chunks[i : i + max_chunks_once_load]
             for i in range(0, len(chunks), max_chunks_once_load)
         ]
         logger.info(
