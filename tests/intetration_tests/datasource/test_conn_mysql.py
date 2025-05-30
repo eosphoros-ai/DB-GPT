0188d2367bd5798af84d1cf78bb3438acb7615ee
Fangyin Cheng <staneyffer@gmail.com>
2025-05-30 23:43:22 +0800

    feat(model): Support Qwen2.5 VL models (#2743)

--- tests/intetration_tests/datasource/test_conn_mysql.py
+++ tests/intetration_tests/datasource/test_conn_mysql.py
@@ -1,21 +1,21 @@
 """
-    Run unit test with command: pytest dbgpt/datasource/rdbms/tests/test_conn_mysql.py
-    docker run -itd --name mysql-test -p 3307:3306 -e MYSQL_ROOT_PASSWORD=12345678 mysql:5.7
-    mysql -h 127.0.0.1 -uroot -p -P3307
-    Enter password:
-    Welcome to the MySQL monitor.  Commands end with ; or \g.
-    Your MySQL connection id is 2
-    Server version: 5.7.41 MySQL Community Server (GPL)
+Run unit test with command: pytest dbgpt/datasource/rdbms/tests/test_conn_mysql.py
+docker run -itd --name mysql-test -p 3307:3306 -e MYSQL_ROOT_PASSWORD=12345678 mysql:5.7
+mysql -h 127.0.0.1 -uroot -p -P3307
+Enter password:
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 2
+Server version: 5.7.41 MySQL Community Server (GPL)
 
-    Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+Copyright (c) 2000, 2023, Oracle and/or its affiliates.
 
-    Oracle is a registered trademark of Oracle Corporation and/or its
-    affiliates. Other names may be trademarks of their respective
-    owners.
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
 
-    Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-    
-    > create database test;
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+> create database test;
 """
 
 import pytest
