69fa83cb18a038988f65d45dbe37548c843bb8c6
Fangyin Cheng <staneyffer@gmail.com>
2025-06-27 17:55:36 +0800

    fix(vis): Fix think message error

--- packages/dbgpt-core/src/dbgpt/vis/tags/vis_thinking.py
+++ packages/dbgpt-core/src/dbgpt/vis/tags/vis_thinking.py
@@ -7,12 +7,12 @@ class VisThinking(Vis):
     @classmethod
     async def build_message(cls, message: str) -> str:
         vis = VisThinking()
-        return f"```{vis.vis_tag()}\n{message}\n```"
+        return f"``````{vis.vis_tag()}\n{message}\n``````"
 
     def sync_display(self, **kwargs) -> str:
         """Display the content using the vis protocol."""
         content = kwargs.get("content")
-        return f"```{self.vis_tag()}\n{content}\n```"
+        return f"``````{self.vis_tag()}\n{content}\n``````"
 
     @classmethod
     def vis_tag(cls):
